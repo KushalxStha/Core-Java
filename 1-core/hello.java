d148dfb64c931a294d5e5e6e3963e18654b7cacd
Kushal Shrestha <shresthakushal61@gmail.com>
2024-08-06 08:46:41 +0545

    removed old files

--- 1-core/hello.java
+++
@@ -1,8 +0,0 @@
-class hello{
-    public static void main(String a[]){
-        int num1=5;
-        int num2=15;
-        int result=num1+num2;
-        System.out.println("The sum is "+result);
-    }
-}
